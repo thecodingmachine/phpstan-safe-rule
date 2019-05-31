e7fa7365ed8fef54105470550d7a20aefdfed42f
vkill <vkill.net@gmail.com>
2019-05-31 09:31:39 +0800

    Ignore json_decode and json_encode when use JSON_THROW_ON_ERROR option
    Fix for composer cs-check
    
    Fix for composer phpstan
    
    Fix for phpunit verbose

--- tests/Rules/UseSafeFunctionsRuleTest.php
+++ tests/Rules/UseSafeFunctionsRuleTest.php
@@ -31,4 +31,22 @@ class UseSafeFunctionsRuleTest extends RuleTestCase
     {
         $this->analyse([__DIR__ . '/data/undirect_call.php'], []);
     }
+
+    public function testJSONDecodeNoCatchSafe()
+    {
+        if (version_compare(PHP_VERSION, '7.3.0', '>=')) {
+            $this->analyse([__DIR__ . '/data/safe_json_decode_for_7.3.0.php'], []);
+        } else {
+            $this->assertTrue(true);
+        }
+    }
+
+    public function testJSONEncodeNoCatchSafe()
+    {
+        if (version_compare(PHP_VERSION, '7.3.0', '>=')) {
+            $this->analyse([__DIR__ . '/data/safe_json_encode_for_7.3.0.php'], []);
+        } else {
+            $this->assertTrue(true);
+        }
+    }
 }
