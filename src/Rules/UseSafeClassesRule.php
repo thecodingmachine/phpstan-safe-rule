e30b538d6664af29499e0fdec5619cd4387af932
Alessandro Lai <alessandro.lai85@gmail.com>
2024-11-21 22:47:08 +0100

    Refactor and adapt the rules to the new PHPStan APIs

--- src/Rules/UseSafeClassesRule.php
+++ src/Rules/UseSafeClassesRule.php
@@ -5,15 +5,9 @@ namespace TheCodingMachine\Safe\PHPStan\Rules;
 
 use PhpParser\Node;
 use PHPStan\Analyser\Scope;
-use PHPStan\Reflection\FunctionReflection;
-use PHPStan\Reflection\MethodReflection;
 use PHPStan\Rules\Rule;
-use PHPStan\ShouldNotHappenException;
+use TheCodingMachine\Safe\PHPStan\Rules\Error\SafeClassRuleError;
 use TheCodingMachine\Safe\PHPStan\Utils\ClassListLoader;
-use TheCodingMachine\Safe\PHPStan\Utils\FunctionListLoader;
-use PhpParser\Node\Arg;
-use PhpParser\Node\Expr;
-use PhpParser\Node\Scalar;
 
 /**
  * This rule checks that no "unsafe" classes are instantiated in code.
@@ -27,11 +21,6 @@ class UseSafeClassesRule implements Rule
         return Node\Expr\New_::class;
     }
 
-    /**
-     * @param Node\Expr\New_ $node
-     * @param \PHPStan\Analyser\Scope $scope
-     * @return string[]
-     */
     public function processNode(Node $node, Scope $scope): array
     {
         $classNode = $node->class;
@@ -43,7 +32,9 @@ class UseSafeClassesRule implements Rule
         $unsafeClasses = ClassListLoader::getClassList();
 
         if (isset($unsafeClasses[$className])) {
-            return ["Class $className is unsafe to use. Its methods can return FALSE instead of throwing an exception. Please add 'use Safe\\$className;' at the beginning of the file to use the variant provided by the 'thecodingmachine/safe' library."];
+            return [
+                new SafeClassRuleError($classNode, $node->getStartLine()),
+            ];
         }
 
         return [];
