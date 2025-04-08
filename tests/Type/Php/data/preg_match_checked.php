051f03c3edaf5bb3bde497ecf8998620532cc515
Benedikt Franke <benedikt.franke@mll.com>
2025-04-08 10:29:39 +0200

    Fix tests with latest dependencies

--- tests/Type/Php/data/preg_match_checked.php
+++ tests/Type/Php/data/preg_match_checked.php
@@ -8,7 +8,7 @@ $string = 'Hello World';
 
 // when the return value is checked, we should have matches,
 // unless the match-group itself is optional
-$type = "array{0: string, 1: non-empty-string, 2: 'o', 3?: 'World'}";
+$type = "array{0: non-falsy-string, 1: non-empty-string, 2: 'o', 3?: 'World'}";
 
 // @phpstan-ignore-next-line - use of unsafe is intentional
 if(\preg_match($pattern, $string, $matches)) {
