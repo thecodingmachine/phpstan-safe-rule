8345add30a2dd1d61485d17bf157cd5677244dc1
Benedikt Franke <benedikt.franke@mll.com>
2025-04-08 15:05:59 +0200

    fix phpstan and folder name

--- tests/Rules/UseSafeCallablesRuleTest.php
+++ tests/Rules/UseSafeCallablesRuleTest.php
@@ -17,7 +17,7 @@ class UseSafeCallablesRuleTest extends RuleTestCase
 
     public function testUnsafe(): void
     {
-        $this->analyse([__DIR__ . '/UseSafeCallables/unsafe.php'], [
+        $this->analyse([__DIR__ . '/UseSafeCallablesRule/unsafe.php'], [
             [
                 "Function json_encode is unsafe to use. It can return FALSE instead of throwing an exception. Please add 'use function Safe\\json_encode;' at the beginning of the file to use the variant provided by the 'thecodingmachine/safe' library.",
                 3,
@@ -27,16 +27,16 @@ class UseSafeCallablesRuleTest extends RuleTestCase
 
     public function testUseSafe(): void
     {
-        $this->analyse([__DIR__ . '/UseSafeCallables/use_safe.php'], []);
+        $this->analyse([__DIR__ . '/UseSafeCallablesRule/use_safe.php'], []);
     }
 
     public function testNativeSafe(): void
     {
-        $this->analyse([__DIR__ . '/UseSafeCallables/native_safe.php'], []);
+        $this->analyse([__DIR__ . '/UseSafeCallablesRule/native_safe.php'], []);
     }
 
     public function testExpr(): void
     {
-        $this->analyse([__DIR__ . '/UseSafeCallables/expr.php'], []);
+        $this->analyse([__DIR__ . '/UseSafeCallablesRule/expr.php'], []);
     }
 }
