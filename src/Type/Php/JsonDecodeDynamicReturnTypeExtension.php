a8d9367cc25f840fdae5e0415cb9b04c1f452e7e
Kayw <weikaiii@sina.cn>
2025-11-21 16:20:27 +0800

    Fix bootstrapFiles not loaded due to early constructor reflection

--- src/Type/Php/JsonDecodeDynamicReturnTypeExtension.php
+++ src/Type/Php/JsonDecodeDynamicReturnTypeExtension.php
@@ -18,13 +18,10 @@ use PHPStan\Type\Type;
  */
 final class JsonDecodeDynamicReturnTypeExtension implements DynamicFunctionReturnTypeExtension
 {
-    private FunctionReflection $nativeJsonDecodeReflection;
-
     public function __construct(
         private readonly JsonThrowOnErrorDynamicReturnTypeExtension $phpstanCheck,
-        ReflectionProvider $reflectionProvider,
+        private readonly ReflectionProvider $reflectionProvider,
     ) {
-        $this->nativeJsonDecodeReflection = $reflectionProvider->getFunction(new Name('json_decode'), null);
     }
 
     public function isFunctionSupported(FunctionReflection $functionReflection): bool
@@ -34,7 +31,9 @@ final class JsonDecodeDynamicReturnTypeExtension implements DynamicFunctionRetur
 
     public function getTypeFromFunctionCall(FunctionReflection $functionReflection, FuncCall $functionCall, Scope $scope): Type
     {
-        $result = $this->phpstanCheck->getTypeFromFunctionCall($this->nativeJsonDecodeReflection, $functionCall, $scope);
+        $nativeJsonDecodeReflection = $this->reflectionProvider->getFunction(new Name('json_decode'), null);
+
+        $result = $this->phpstanCheck->getTypeFromFunctionCall($nativeJsonDecodeReflection, $functionCall, $scope);
 
         // if PHPStan reports null and there is a json error, then an invalid constant string was passed
         if ($result->isNull()->yes() && JSON_ERROR_NONE !== json_last_error()) {
